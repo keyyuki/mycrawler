ddf6e2645b00f0348a58bc4956ad7f7f95216764
Nguyễn Ngoc Kiên <keyyuki@users.noreply.github.com>
2017-07-24 22:55:53 +0700

    jquery server

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 const http = require("http");
 var router = express.Router();
+const jsdom = require("jsdom");
+const dom = new jsdom.JSDOM(`<!DOCTYPE html>`);
+var $ = require('jquery')(dom.window);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -49,6 +52,83 @@ router.get('/crawlerhosocty', function(request, response) {
     req.end();
 
 });
+router.get('/crawlerdetail', function(request, response) {
+
+
+    var options = {
+        host: "www.hosocongty.vn",
+
+        method: 'GET',
+        path: '/cong-ty-tnhh-dich-vu-thuong-mai-phuc-minh-hong-an-com-1558406.htm'
+    };
+    var req = http.request(options, function(res) {
+        var chunks = [];
+
+
+        res.on("data", function(chunk) {
+            var str = chunk.toString('utf8');
+            chunks.push(str);
+        });
+
+        res.on("end", function() {
+            //var body = Buffer.concat(chunks);
+            rs = chunks.join('').toString();
+            innerBodyAsString = /<body[\s\S]*?>([\s\S]*)<\/body>/g.exec(rs)[1];
+            response.send(innerBodyAsString);
+
+            //console.log(body);
+            //console.log(chunks);
+        });
+        res.on("error", function(chunk) {
+            response.send('http error');
+        });
+    });
+    req.end();
+})
+
+router.get('/crawlergov', function(request, response) {
+    var options = {
+        host: "online.gov.vn",
+
+        method: 'GET',
+        path: '/PersonalWebsiteView.aspx'
+    };
+    var req = http.request(options, function(res) {
+        var chunks = [];
+
+
+        res.on("data", function(chunk) {
+            var str = chunk.toString('utf8');
+            chunks.push(str);
+        });
+
+        res.on("end", function() {
+            //var body = Buffer.concat(chunks);
+            rs = chunks.join('').toString();
+            innerBodyAsString = /<body[\s\S]*?>([\s\S]*)<\/body>/g.exec(rs)[1];
+            var result = []
+            $(rs).find('#ctl00_ContentPlaceHolder1_gridWebsites tbody tr').each(function() {
+                if (!$(this).find('td').length) {
+                    return null;
+                }
+                var item = {}
+                item.website = $(this).find('td:eq(1) a').attr('href');
+                item.name = $(this).find('td:eq(2)').text().trim();
+                item.link = 'http://online.gov.vn' + $(this).find('td:eq(2) a').attr('href');
+                result.push(item);
+            })
+            response.send(JSON.stringify(result));
+
+            //console.log(body);
+            //console.log(chunks);
+        });
+        res.on("error", function(chunk) {
+            response.send('http error');
+        });
+    });
+    req.end();
+});
+
 
 // innerBodyAsString=/<body[\s\S]*?>([\s\S]*)<\/body>/g.exec(rs)[1];
 
